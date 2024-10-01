60a93341db7ec49bf0818aac779682579b2c32e6
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-01 20:25:11 +0530

    Time: 60 ms (10.99%), Space: 49 MB (37.49%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    // return arr.map((v, i) => fn(v, i));
+    let tempArray = [];
+        for (let i = 0; i < arr.length; i++) {
+        tempArray.push(fn(arr[i], i));
+    }
+    return tempArray;
+};
