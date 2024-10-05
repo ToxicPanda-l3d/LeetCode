8cd9422ee7ef2433d9becb97111d53de77a70935
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-05 20:32:07 +0530

    Time: 42 ms (95.77%), Space: 48.4 MB (88.17%) - LeetHub

--- 2666-allow-one-function-call/2666-allow-one-function-call.js
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -3,19 +3,16 @@
  * @return {Function}
  */
 var once = function(fn) {
-    // Initialize a flag to keep track of whether the function has been called
-    let called = false;
-    
-    // Return a new function that will be called in place of the original function
+    let hasBeenCalled = false; // Track if the function has been called
+    let result; // Store the result of the first call
+
     return function(...args) {
-        // If the function has already been called, return undefined
-        if (called) return undefined;
-        
-        // Mark the function as called so it won't run again
-        called = true;
-        
-        // Call the original function with the arguments and return the result
-        return fn(...args);
+        if (!hasBeenCalled) {
+            hasBeenCalled = true; // Mark the function as called
+            result = fn.apply(this, args); // Call the original function
+            return result; // Return the result of the first call
+        }
+        return undefined; // Return undefined for subsequent calls
     };
 };
 
