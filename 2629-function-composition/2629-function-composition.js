8153442e8a2783205416023fda02c247336e97da
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-05 20:07:54 +0530

    Time: 58 ms (84.14%), Space: 50.2 MB (35.40%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -10,7 +10,6 @@ var compose = function(functions) {
         return functions.reduceRight((acc, fn) => fn(acc), x);
     }
 };
-
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
