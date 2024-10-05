aa77ad70fb99c01ad89c7e768e5444bd04d3babd
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-05 20:31:12 +0530

    Time: 60 ms (13.93%), Space: 49 MB (22.61%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let hasBeenCalled = false; // Track if the function has been called
+    let result; // Store the result of the first call
+
+    return function(...args) {
+        if (!hasBeenCalled) {
+            hasBeenCalled = true; // Mark the function as called
+            result = fn.apply(this, args); // Call the original function
+            return result; // Return the result of the first call
+        }
+        return undefined; // Return undefined for subsequent calls
+    };
+};
+
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
