21ac86438a3b931c302b9e6a79ffb95105c4d523
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-08 22:04:13 +0530

    Time: 65 ms (26.52%), Space: 49.9 MB (11.03%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -5,13 +5,17 @@
  */
 var timeLimit = function(fn, t) {
     return async function(...args) {
-        return Promise.race([
-            fn(...args),
-            new Promise((_, reject) => setTimeout(() => reject('Time Limit Exceeded'), t))
-        ]);
+        // Create a reusable timeout Promise
+        return new Promise((resolve, reject) => {
+            const timeoutId = setTimeout(() => reject('Time Limit Exceeded'), t);
+            
+            fn(...args)
+                .then(resolve)
+                .catch(reject)
+                .finally(() => clearTimeout(timeoutId)); // Clean up timeout once done
+        });
     };
 };
-
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
  * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
