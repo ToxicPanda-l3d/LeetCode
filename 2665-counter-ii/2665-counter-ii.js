de5017364ec7797ed10f44a692099e40fc54c1dc
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-10 00:36:59 +0530

    Time: 49 ms (94.27%), Space: 51.1 MB (94.11%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -2,6 +2,7 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
+
 var createCounter = function(init) {
     let count = init;
     // Return an object with three methods: increment, decrement, and reset.
