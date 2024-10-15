1b1c059cdf3cb3f9165e1afe80caa9706c2e057d
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-15 21:26:53 +0530

    Time: 1 ms (99.97%), Space: 42.3 MB (24.61%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,33 +1,35 @@
 class Solution {
-    private Boolean[][] f;
+    private Boolean[][] f; // Memoization table to store results of subproblems
     private String s;
     private String p;
-    private int m;
-    private int n;
+    private int m; // Length of string s
+    private int n; // Length of pattern p
 
     public boolean isMatch(String s, String p) {
         m = s.length();
         n = p.length();
-        f = new Boolean[m + 1][n + 1];
+        f = new Boolean[m + 1][n + 1]; // Initialize memoization table
         this.s = s;
         this.p = p;
-        return dfs(0, 0);
+        return dfs(0, 0); // Start DFS from the beginning of both string and pattern
     }
 
     private boolean dfs(int i, int j) {
         if (j >= n) {
-            return i == m;
+            return i == m; // If pattern is exhausted, check if string is also exhausted
         }
         if (f[i][j] != null) {
-            return f[i][j];
+            return f[i][j]; // Return memoized result if already computed
         }
         boolean res = false;
         if (j + 1 < n && p.charAt(j + 1) == '*') {
-            res = dfs(i, j + 2)
+            // Two possibilities: ignore the '*' or use it to match characters
+            res = dfs(i, j + 2) 
                 || (i < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && dfs(i + 1, j));
         } else {
+            // Regular character match or '.' wildcard
             res = i < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') && dfs(i + 1, j + 1);
         }
-        return f[i][j] = res;
+        return f[i][j] = res; // Store the result in the memoization table
     }
 }
