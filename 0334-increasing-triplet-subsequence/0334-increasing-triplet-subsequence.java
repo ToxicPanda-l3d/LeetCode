e5388e64e95965864438ab3d6b934d0fbf7f52d8
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-18 23:35:28 +0530

    Time: 2 ms (91.64%), Space: 133.9 MB (49.79%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,27 +1,16 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int n = nums.length; // Get the length of the input array
-        if (n < 3) return false; // A triplet cannot exist if there are fewer than 3 numbers
-        
-        // Initialize two variables to track the first and second minimums
-        int min1 = Integer.MAX_VALUE; // First minimum
-        int min2 = Integer.MAX_VALUE; // Second minimum
-
-        // Loop through each number in the array
+        int min = Integer.MAX_VALUE, mid = Integer.MAX_VALUE;
         for (int num : nums) {
-            if (num <= min1) {
-                // Update min1 if the current number is smaller or equal
-                min1 = num;
-            } else if (num <= min2) {
-                // Update min2 if the current number is greater than min1 but smaller or equal to min2
-                min2 = num;
-            } else {
-                // If we find a number greater than both min1 and min2, we have an increasing triplet
+            if (num > mid) {
                 return true;
             }
+            if (num <= min) {
+                min = num;
+            } else {
+                mid = num;
+            }
         }
-        
-        // If no increasing triplet is found, return false
         return false;
     }
 }
