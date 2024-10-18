d2e69a7f96c0a1ba7d1e25f428a5e08fec8a98ff
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-18 23:39:02 +0530

    Time: 23 ms (100%), Space: 36.4 MB (25.27%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,11 +1,17 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        mi, mid = inf, inf
+        mi = inf
+        mid = inf
+        
         for num in nums:
             if num > mid:
+                # Found a number greater than mid, thus we have an increasing triplet
                 return True
-            if num <= mi:
-                mi = num
-            else:
+            elif num > mi:
+                # Found a number greater than mi but less than or equal to mid
                 mid = num
+            else:
+                # Update mi to the smallest number seen so far
+                mi = num
+        
         return False
