3d4173f1da63a08954e9dc46a60a96f7812212d3
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-19 22:21:01 +0530

    Time: 1 ms (99.45%), Space: 44.2 MB (24.34%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,20 +1,20 @@
-
 class Solution {
     public int compress(char[] chars) {
-        int k = 0, n = chars.length;
+        int k = 0, n = chars.length;  // k: write index, n: length of chars
         for (int i = 0, j = i + 1; i < n;) {
+            // Find the end of the sequence of the same characters
             while (j < n && chars[j] == chars[i]) {
                 ++j;
             }
-            chars[k++] = chars[i];
-            if (j - i > 1) {
-                String cnt = String.valueOf(j - i);
+            chars[k++] = chars[i];  // Write the character
+            if (j - i > 1) {  // If there is more than one occurrence
+                String cnt = String.valueOf(j - i);  // Convert count to string
                 for (char c : cnt.toCharArray()) {
-                    chars[k++] = c;
+                    chars[k++] = c;  // Write each digit of the count
                 }
             }
-            i = j;
+            i = j;  // Move to the next group of characters
         }
-        return k;
+        return k;  // Return the length of the compressed array
     }
 }
