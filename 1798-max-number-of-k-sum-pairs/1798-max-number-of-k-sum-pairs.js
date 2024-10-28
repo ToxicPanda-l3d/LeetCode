7c8721e1dce446bc9a12201e3693cb6b1ba89e3e
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-28 20:13:29 +0530

    .

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.js
+++
@@ -1,8 +0,0 @@
-var maxOperations = function(nums, k) {
-    let m = new Map(), ans = 0
-    for (let n of nums)
-        if (n < k)
-            if (m.get(k - n)) m.set(k - n, m.get(k - n) - 1), ans++
-            else m.set(n, (m.get(n) || 0) + 1)
-    return ans
-};
