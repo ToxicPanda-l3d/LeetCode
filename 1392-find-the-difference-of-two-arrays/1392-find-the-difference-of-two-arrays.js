ad759e47f11ceebf040b8d370d832c4e33f3b3bd
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-30 13:37:53 +0530

    Delete 1392-find-the-difference-of-two-arrays directory

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++
@@ -1,7 +0,0 @@
-var findDifference = function (nums1, nums2) {
-    const s1 = new Set(nums1);
-    const s2 = new Set(nums2);
-    nums1.forEach(num => s2.delete(num));
-    nums2.forEach(num => s1.delete(num));
-    return [Array.from(s1), Array.from(s2)];
-};
