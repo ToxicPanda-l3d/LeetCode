1d29349b83b37748e8d8ad6144d67f57e5c0fb6c
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-10-30 13:36:12 +0530

    Time: 7 ms (93.58%), Space: 54.56 MB (92.80%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -1,8 +1,3 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[][]}
- */
 var findDifference = function (nums1, nums2) {
     const s1 = new Set(nums1);
     const s2 = new Set(nums2);
